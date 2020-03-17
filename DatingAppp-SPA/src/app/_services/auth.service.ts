e38241900ec53029d23e7730558175ffe80458cc
anga4747 <anga4747@colorado.edu>
2020-03-17 14:21:51 -0600

    Installed Angular JWT for Token Validation -client

--- DatingAppp-SPA/src/app/_services/auth.service.ts
+++ DatingAppp-SPA/src/app/_services/auth.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {map} from 'rxjs/operators';
+import {JwtHelperService} from '@auth0/angular-jwt';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
 constructor(private http: HttpClient) { }
 
 login(model: any) {
@@ -24,4 +26,9 @@ register(model: any) {
   return this.http.post(this.baseUrl + 'register', model); // new register method
 }
 
+loggedIn() {
+  const token = localStorage.getItem('token');
+  return !this.jwtHelper.isTokenExpired(token);
+
+}
 }
